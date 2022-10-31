b70a6894e2fdbe73e500374ed1291ee109767a93
Shrikeshs <shrikeshs98@gmail.com>
2022-10-31 18:54:36 +0530

    Added asynio support for aggregations

--- app/reports/api.py
+++ app/reports/api.py
@@ -4,26 +4,27 @@ import json
 from bson.json_util import dumps
 from flask import request, make_response, Blueprint
 
-from app.util import list_of_reports
+from app.reports.util import list_of_reports
 
 report_blueprint = Blueprint("report", __name__, url_prefix='/reports')
 
 
+# report name change,  top k , params with 5/10/50
+
+# util to be moved to reports pack
 @report_blueprint.route('/', methods=['POST'])
 async def report_generator():
     report_name = request.json["report_name"]
     report_obj = list_of_reports[report_name]
     report_obj.insert_report_status(report_name, 'pending')
     reports_ = asyncio.create_task(report_obj.generate_reports())
-    await reports_
     report_obj.insert_report_status(report_name, 'done')
-    print(type(reports_.result()))
-    response = make_response(dumps(reports_.result()), 200)
+    response = make_response(dumps(reports_.result()), 200) # 202 , accepted
     response.headers["Content-Type"] = "application/json"
     return response
 
 
-@report_blueprint.route('/status', methods=['POST'])
+@report_blueprint.route('/<id>', methods=['POST'])
 async def report_status():
     report_name = request.json["report_name"]
     report_obj = list_of_reports[report_name]
