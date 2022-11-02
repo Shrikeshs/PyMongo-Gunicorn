541ba412a6f1b3b4f1939e781a28ab8819bb1301
Shrikeshs <shrikeshs98@gmail.com>
2022-11-02 18:20:20 +0530

    Added asynio support for aggregations

--- app/reports/api.py
+++ app/reports/api.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 import threading
 
 from bson import ObjectId
@@ -15,13 +16,15 @@ work_queue = asyncio.Queue(5)
 @report_blueprint.route('/', methods=['POST'])
 async def report_generator():
     if work_queue.full():
+        logging.debug('Worker is full...')
         abort(400, description="Worker is full, please try after sometime")
     report_name = request.json["report_name"]
     request_args = request.args
     k = request_args["k"]
     report_obj = list_of_reports[report_name]
-    from app.main import loop
+    from app.util import loop
     report_status_task = insert_report_status(report_name, 'pending', [])
+    logging.info('Report has been inserted to MongoDb')
     reports_task = loop.create_task(report_obj.generate_reports(k, report_status_task.inserted_id))
     work_queue.put_nowait(reports_task)
     response = make_response(dumps(report_status_task.inserted_id), 202)
@@ -37,7 +40,7 @@ async def worker_fn():
 
 
 def run_periodically():
-    from app.main import loop
+    from app.util import loop
     loop.run_until_complete(loop.create_task(worker_fn()))
     threading.Timer(10, run_periodically).start()
 
@@ -47,8 +50,8 @@ run_periodically()
 
 @report_blueprint.route('/<id>', methods=['POST'])
 def report_status(id):
-    print(f"In flask rest api level in repor tstats: {threading.current_thread().name}")
     report_name = request.json["report_name"]
+    logging.info('Retrieving report for id : ' + str(id))
     id_ = get_report_status({'name': str(report_name), "_id": ObjectId(id)})
     response = make_response(dumps(id_), 200)
     response.headers["Content-Type"] = "application/json"
